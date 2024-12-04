1252a9dbc74e7191240de7e2741a77d5117fdf6e
Stuart Walton <stuart@stickypiston.co>
2024-12-04 20:32:43 +0000

    Add run function

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,4 +1,13 @@
 defmodule Day2 do
+  def run(src, part) do
+    case {src, part} do
+      {:test, 1} -> data_example_txt() |> part1()
+      {:test, 2} -> data_example_txt() |> part2()
+      {:input, 1} -> data_txt() |> part1()
+      {:input, 2} -> data_txt() |> part2()
+    end
+  end
+
   def data_example_txt() do
     Application.app_dir(:aoc2024, "/priv/day2_example.txt")
   end
