934e156bd8b72c27fccffad2dce2cf54b2438764
Stuart Walton <stuart@stickypiston.co>
2024-12-05 00:53:32 +0000

    Use Integer.parse and format cases

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,4 +1,7 @@
 defmodule Day3 do
+
+  @number_chars ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
   def run(src, part) do
     case {src, part} do
       {:test, 1} -> data_example_txt() |> part1()
@@ -43,10 +46,14 @@ defmodule Day3 do
       fn acc ->
         case acc do
           "" -> {:halt, acc}
-          "mul" <> rest -> {parse_mul_args(rest) |> Enum.to_list(), rest}
-          "do()" <> rest -> {[:do], rest}
-          "don't()" <> rest -> {[:dont], rest}
-          _ -> {[], String.slice(acc, 1..-1//1)}
+          "mul" <> rest ->
+            {parse_mul_args(rest) |> Enum.to_list(), rest}
+          "do()" <> rest ->
+            {[:do], rest}
+          "don't()" <> rest ->
+            {[:dont], rest}
+          _ ->
+            {[], String.slice(acc, 1..-1//1)}
         end
       end,
       fn _acc -> nil end
@@ -57,33 +64,28 @@ defmodule Day3 do
     Stream.resource(
       fn -> {content, :open, 0, 0, 0, 0} end,
       fn acc ->
-        case acc do
-          {"", _mode, _arg1, _count1, _arg2, _count2} -> {:halt, acc}
-          {_content, :done, _arg1, _count1, _arg2, _count2} -> {:halt, acc}
-          {"(" <> rest, :open, arg1, count1, arg2, count2} -> {[], {rest, :arg1, arg1, count1, arg2, count2}}
-          {"0" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, arg1 * 10, count1 + 1, arg2, count2}}
-          {"1" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 1, count1 + 1, arg2, count2}}
-          {"2" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 2, count1 + 1, arg2, count2}}
-          {"3" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 3, count1 + 1, arg2, count2}}
-          {"4" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 4, count1 + 1, arg2, count2}}
-          {"5" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 5, count1 + 1, arg2, count2}}
-          {"6" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 6, count1 + 1, arg2, count2}}
-          {"7" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 7, count1 + 1, arg2, count2}}
-          {"8" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 8, count1 + 1, arg2, count2}}
-          {"9" <> rest, :arg1, arg1, count1, arg2, count2} when count1 < 3 -> {[], {rest, :arg1, (arg1 * 10) + 9, count1 + 1, arg2, count2}}
-          {"," <> rest, :arg1, arg1, count1, arg2, count2} when count1 > 0 -> {[], {rest, :arg2, arg1, count1, arg2, count2}}
-          {"0" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, arg2 * 10, count2 + 1}}
-          {"1" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 1, count2 + 1}}
-          {"2" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 2, count2 + 1}}
-          {"3" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 3, count2 + 1}}
-          {"4" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 4, count2 + 1}}
-          {"5" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 5, count2 + 1}}
-          {"6" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 6, count2 + 1}}
-          {"7" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 7, count2 + 1}}
-          {"8" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 8, count2 + 1}}
-          {"9" <> rest, :arg2, arg1, count1, arg2, count2} when count2 < 3 -> {[], {rest, :arg2, arg1, count1, (arg2 * 10) + 9, count2 + 1}}
-          {")" <> rest, :arg2, arg1, count1, arg2, count2} when count2 > 0 -> {[{:mul, arg1, arg2}], {rest, :done, arg1, count1, arg2, count2}}
-          _ -> {:halt, acc}
+        {content, mode, arg1, count1, arg2, count2} = acc
+        next = String.first(content)
+        rest = String.slice(content, 1..-1//1)
+        case {next, mode} do
+          {nil, _mode} ->
+            {:halt, acc}
+          {_content, :done} ->
+            {:halt, acc}
+          {"(", :open} ->
+            {[], {rest, :arg1, arg1, count1, arg2, count2}}
+          {num, :arg1} when count1 < 3 and num in @number_chars ->
+            {val, _rem} = Integer.parse(num)
+            {[], {rest, :arg1, (arg1 * 10) + val, count1 + 1, arg2, count2}}
+          {",", :arg1} when count1 > 0 ->
+            {[], {rest, :arg2, arg1, count1, arg2, count2}}
+          {num, :arg2} when count2 < 3 and num in @number_chars ->
+            {val, _rem} = Integer.parse(num)
+            {[], {rest, :arg2, arg1, count1, (arg2 * 10) + val, count2 + 1}}
+          {")", :arg2} when count2 > 0 ->
+            {[{:mul, arg1, arg2}], {rest, :done, arg1, count1, arg2, count2}}
+          _ ->
+            {:halt, acc}
         end
       end,
       fn _acc -> nil end
@@ -94,8 +96,10 @@ defmodule Day3 do
     operations
     |> Stream.map(fn op ->
       case op do
-        {:mul, arg1, arg2} -> arg1 * arg2
-        _ -> 0
+        {:mul, arg1, arg2} ->
+          arg1 * arg2
+        _ ->
+          0
       end
     end)
   end
@@ -104,11 +108,16 @@ defmodule Day3 do
     operations
     |> Stream.transform(do_add, fn op, acc ->
       case op do
-        {:mul, arg1, arg2} when acc -> {[arg1 * arg2], acc}
-        {:mul, _arg1, _arg2} -> {[], acc}
-        :do -> {[], true}
-        :dont -> {[], false}
-        _ -> {:halt, acc}
+        {:mul, arg1, arg2} when acc ->
+          {[arg1 * arg2], acc}
+        {:mul, _arg1, _arg2} ->
+          {[], acc}
+        :do ->
+          {[], true}
+        :dont ->
+          {[], false}
+        _ ->
+          {:halt, acc}
       end
     end)
   end
